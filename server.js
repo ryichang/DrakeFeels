1087d93431084e8eb2bace12da89b0b73f2a64db
ryichang <ryichang@gmail.com>
2016-01-12 13:18:12 -0800

    Change setup for deployment

---
+++ server.js
@@ -0,0 +1,9 @@
+// server.js
+
+// mongoose.connect(
+//   process.env.MONGOLAB_URI ||
+//   process.env.MONGOHQ_URL ||
+//   'mongodb://localhost/YOUR_LOCAL_DATABASE_NAME' // plug in the db name you've been using
+// );
+
+app.listen(process.env.PORT || 3000);
