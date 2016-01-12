3acb98cb2092e1e16b8493ef0324449a350e22a9
ryichang <ryichang@gmail.com>
2016-01-12 14:10:53 -0800

    Changed index.html to home.html and added PHP command

--- server.js
+++
@@ -1,9 +0,0 @@
-// server.js
-
-// mongoose.connect(
-//   process.env.MONGOLAB_URI ||
-//   process.env.MONGOHQ_URL ||
-//   'mongodb://localhost/YOUR_LOCAL_DATABASE_NAME' // plug in the db name you've been using
-// );
-
-app.listen(process.env.PORT || 3000);
